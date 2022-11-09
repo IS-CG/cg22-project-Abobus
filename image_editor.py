dc067dac293a70715de76e3917d14d83a9dceac1
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2022-11-10 00:27:37 +0300

    add method

--- image_editor.py
+++ image_editor.py
@@ -81,6 +81,13 @@ def save():
         img.save(imgname)
 
 
+def adjust_gamma(image: np.ndarray, gamma: float=1.0) -> np.ndarray:
+    inv_gamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** inv_gamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, table)
+
+
 def change_to_rgb(display=True):
     global img_array
     global COLOR

5469db9f6f0fad6b5c977e254de9b7d2b5d038a8
Suslov Mikhail <misha.suslov.2016@mail.ru>
2022-11-15 22:00:01 +0300

    feat: add docs to geometric transforms

--- lib/image_transforms/img_format_transformer.py
+++ lib/image_transforms/img_format_transformer.py
@@ -6,13 +6,24 @@ from lib.image_managers import ImageViewer
 
 
 class ImgFormatTransformer:
+    """
+    A class that doing some geometric transformation with image
+    """
     @staticmethod
     def rotate():  # TODO: поменять на numpy
+        """
+        Rotates the image 90 degrees
+        :return: None
+        """
         ImageObjectSingleton.img = ImageObjectSingleton.img.rotate(90)
         ImageViewer.display_img()
 
     @staticmethod
     def flip():  # TODO: поменять на numpy
+        """
+        It mirrors the image horizontally
+        :return: None
+        """
         ImageObjectSingleton.img = ImageObjectSingleton.img.transpose(Image.FLIP_LEFT_RIGHT)
         ImageViewer.display_img()
 

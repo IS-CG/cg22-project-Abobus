22da225b3fadf39dc8435a990b876599b5b060d5
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2022-11-20 21:59:49 +0300

    delete legacy

--- main_runner.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-from lib.singleton_objects import UIBuilder
-
-with open('ui_config.yaml', 'r') as f:
-    ui_conf = yaml.safe_load(f)
-
-if __name__ == "__main__":
-    UIBuilder.build_and_run()

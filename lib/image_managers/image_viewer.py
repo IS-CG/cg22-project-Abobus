16c48fe98273ce94e7ff7fe3ebe88de6e1584bb1
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2022-11-22 22:23:59 +0300

    add image preview method

--- lib/image_managers/image_viewer.py
+++ lib/image_managers/image_viewer.py
@@ -1,4 +1,7 @@
+from functools import partial
+
 from PIL import Image, ImageTk
+import tkinter as tk
 import numpy as np
 import cv2
 
@@ -24,6 +27,21 @@ class ImageViewer:
             new_h = e.height if e.height < h else h
             resized_bg = cv2.resize(img, (new_w, new_h))
             cls.display_img_array(resized_bg)
+
+    @classmethod
+    def preview_img(cls, data: np.ndarray) -> None:
+        top = tk.Toplevel(UISingleton.ui_main)
+        top.geometry("700x250")
+        top.title("Child Window")
+        child_menu = tk.Menu(top)
+        top.config(menu=child_menu)
+        child_menu.add_command(label='Save Current Img', command=partial(cls.display_img_array, data))
+        canvas = tk.Canvas(top, highlightthickness=0, width=600, height=400)
+        canvas.pack(fill=tk.BOTH, expand=1)
+        img_box = canvas.create_image(0, 0, image=None, anchor='nw')
+        img = ImageTk.PhotoImage(Image.fromarray(data))
+        canvas.itemconfig(img_box, image=img)
+        canvas.image = img
 
     @classmethod
     def display_img_array(cls, data: np.ndarray) -> None:

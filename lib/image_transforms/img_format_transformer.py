8dcf09f57ee62c546a2c2628dea80255d19a9cde
vaskers5 <48238046+vaskers5@users.noreply.github.com>
2022-12-04 19:56:37 +0100

    optimized billinear resize

--- lib/image_transforms/img_format_transformer.py
+++ lib/image_transforms/img_format_transformer.py
@@ -3,10 +3,21 @@ from tkinter import simpledialog
 from numba import njit
 import numpy as np
 
-from lib.image_transforms.image_kernels import padding, bicubic_kernel, lanczos_filter
+from lib.image_transforms.image_kernels import bicubic_kernel, lanczos_filter
 from lib.singleton_objects import ImageObjectSingleton, UISingleton
 from lib.image_managers import ImageViewer
 import math
+
+
+
+
+@njit
+def padding(image: np.ndarray, padding_size: int) -> np.ndarray: # todo перенеси вместо своего паддинга, я не хочу все твои алгосы рефакторить :3
+    height, width = image.shape[:2]
+    new_image = np.zeros((height + padding_size * 2, width + padding_size * 2, 3), dtype=np.uint8)
+    new_image[padding_size:height + padding_size, padding_size:width + padding_size] = image
+    return new_image
+
 
 
 class ImgFormatTransformer:
@@ -69,40 +80,42 @@ class ImgFormatTransformer:
         ImageObjectSingleton.img_array = img_array
         ImageViewer.display_img_array(ImageObjectSingleton.img_array)
 
-    @staticmethod
-    def bilinear_resize():
-        image = ImageObjectSingleton.img_array
-        img_height, img_width = image.shape[:2]
-        factor = simpledialog.askfloat(title="Type a factor value", prompt="Try not to use big values",
-                                       parent=UISingleton.ui_main)
-        height, width = int(img_height * factor), int(img_width * factor)
-        resized = np.array(Image.new('RGB', (width, height)))
+    @classmethod
+    def bilinear_resize(cls):
 
-        x_ratio = float(img_width - 1) / (width - 1) if width > 1 else 0
-        y_ratio = float(img_height - 1) / (height - 1) if height > 1 else 0
+        img = ImageObjectSingleton.img_array
 
-        for i in range(height):
-            for j in range(width):
-                x_l, y_l = math.floor(x_ratio * j), math.floor(y_ratio * i)
-                x_h, y_h = math.ceil(x_ratio * j), math.ceil(y_ratio * i)
+        new_width = int(simpledialog.askstring("Input", "Enter new width:"))
+        new_height = int(simpledialog.askstring("Input", "Enter new height:"))
 
-                x_weight = (x_ratio * j) - x_l
-                y_weight = (y_ratio * i) - y_l
+        new_center_x = int(simpledialog.askstring("Input", "Enter center cordinate:"))
+        new_center_y = int(simpledialog.askstring("Input", "Enter new center coordinate:"))
 
-                a = image[y_l, x_l]
-                b = image[y_l, x_h]
-                c = image[y_h, x_l]
-                d = image[y_h, x_h]
+        new_img = cls._bilinear_resize(img, new_height, new_width).astype(np.uint8)
 
-                pixel = a * (1 - x_weight) * (1 - y_weight) \
-                        + b * x_weight * (1 - y_weight) + \
-                        c * y_weight * (1 - x_weight) + \
-                        d * x_weight * y_weight
-
-                resized[i][j] = pixel
-
-        ImageObjectSingleton.img_array = resized
+        ImageObjectSingleton.img_array = new_img.astype(np.uint8)
         ImageViewer.display_img_array(ImageObjectSingleton.img_array)
+
+    @staticmethod
+    @njit
+    def _bilinear_resize(image: np.ndarray, new_width: int, new_height: int) -> np.ndarray:
+        height, width = image.shape[:2]
+        image = padding(image, 1)
+        new_image = np.zeros((new_height, new_width, 3), dtype=np.uint8)
+        for i in range(new_height):
+            for j in range(new_width):
+                x = j * width / new_width
+                y = i * height / new_height
+                x1 = math.floor(x)
+                y1 = math.floor(y)
+                x2 = math.ceil(x)
+                y2 = math.ceil(y)
+                x_diff = x - x1
+                y_diff = y - y1
+                new_image[i, j] = (1 - x_diff) * (1 - y_diff) * image[y1, x1] + x_diff * (1 - y_diff) * image[y1, x2] + (
+                        1 - x_diff) * y_diff * image[y2, x1] + x_diff * y_diff * image[y2, x2]
+        return new_image
+            
 
     @staticmethod
     def mitchell():
@@ -169,6 +182,8 @@ class ImgFormatTransformer:
 
         ImageObjectSingleton.img_array = dst.astype(np.uint8)
         ImageViewer.display_img_array(ImageObjectSingleton.img_array)
+    
+
 
     @staticmethod
     def lanczos():

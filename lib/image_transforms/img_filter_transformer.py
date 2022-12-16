9b2d9635fba51136e99a5557d2132374703e155a
Suslov Mikhail <misha.suslov.2016@mail.ru>
2022-12-16 09:10:43 +0300

    update sobel

--- lib/image_transforms/img_filter_transformer.py
+++ lib/image_transforms/img_filter_transformer.py
@@ -174,11 +174,14 @@ class ImgFilterTransformer:
     @staticmethod
     def sobel_filter():
         img = ImageObjectSingleton.img_array
-        r_img, g_img, b_img = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+        if len(img.shape) == 3:
+            r_img, g_img, b_img = img[:, :, 0], img[:, :, 1], img[:, :, 2]
 
-        gamma = 1.400
-        r_const, g_const, b_const = 0.2126, 0.7152, 0.0722
-        grayscale_image = r_const * r_img ** gamma + g_const * g_img ** gamma + b_const * b_img ** gamma
+            gamma = 1.400
+            r_const, g_const, b_const = 0.2126, 0.7152, 0.0722
+            grayscale_image = r_const * r_img ** gamma + g_const * g_img ** gamma + b_const * b_img ** gamma
+        else:
+            grayscale_image = img
         Gx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
         Gy = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
         [rows, columns] = np.shape(grayscale_image)
